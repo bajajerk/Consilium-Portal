f46baeb287bb6da92090a278b3c7d7de5f7b6f63
Mayank <bajaj3743@gmail.com>
2018-01-09 03:17:42 +0530

    Bcase reg done

--- app/controllers/bcases_controller.rb
+++ app/controllers/bcases_controller.rb
@@ -11,37 +11,38 @@ class BcasesController < ApplicationController
   def saveteam
   team=Bcase.new
   team.name=params[:teamname]
-  username1=params[:username1]
-  user1=User.find_by_uniquecode(username1)
+  user1=current_user
 
-  if (user1 && user1.bcase_id.nil?)
-  team.users<<user1
-  else
-    return redirect_to '/home/errorpage'
+    if (user1.bcase_id.nil?)
+    team.users<<user1
+    else
+      return redirect_to '/home/errorpage'
+    end
+
+    if (params[:email2].length>0)
+      email2=params[:email2]
+      user2=User.find_by_email(email2)
+        if (user2 && user2.bcase_id.nil?)
+             team.users<<user2
+        elsif (user2 && !user2.bcase_id.nil?)
+             return redirect_to '/home/errorpage'    
+        else
+             newUser2=User.new
+             newUser2.email=email2
+             newUser2.name=params[:name2]
+             newUser2.collegename=params[:collegename2]
+             newUser2.phone=params[:phone]
+             newUser2.password='conspassword'
+             newUser2.save
+             team.users<<newUser2
+             eventname='NETAJI SUBHAS BUSINESS CASE'
+             Teamcreated.newusermade(newUser2.email,newUser2.name,eventname).deliver_now
+        end
+    end
+
+    team.save
+    Teamcreated.mockstock(user1.email,user1.name).deliver_now
+    return redirect_to '/home/index'
   end
 
-  if (params[:username2].length>0)
-    username2=params[:username2]
-    user2=User.find_by_uniquecode(username2)
-      if (user2 && user2.bcase_id.nil?)
-           team.users<<user2
-      else
-           return redirect_to '/home/errorpage'
-      end
-  end
-
-  if (params[:username3].length>0)
-    username3=params[:username3]
-    user3=User.find_by_uniquecode(username3)
-      if (user3 && user3.bcase_id.nil?)
-           team.users<<user3
-      else
-           return redirect_to '/home/errorpage'
-      end
-  end
-
-  team.save
-  return redirect_to '/home/index'
-  end
- 
 end
